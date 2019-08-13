9a321273e54bdd32e616af72cbf225c4a1edeb57
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-08-12 20:14:26 -0700

    Additional code changes for session management

--- ngacertification.php
+++ ngacertification.php
@@ -2,7 +2,7 @@
 include 'templates/header.php';
 include 'dbconn.php';
 
-$getAgenciesQuery = 'select id, agencyname from govtagency where govtagencyclassid=5';
+$getAgenciesQuery = 'select id, agencyname from govtagency where govtagencyclassid=5 order by agencyname';
 $agencyStmt= $dbh->query($getAgenciesQuery);
 //$agencyResult = $agencyStmt->fetchAll();
 //print_r($agencyResult);
---
+++ templates/magic.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+if(isset($_SESSION['userId']) && isset($_SESSION['realName']))
+{
+    $loggedInUserId= $_SESSION['userId'];
+    $loggedInUserRealname= $_SESSION['realName'];
+    $loggedInAccessLevel= $_SESSION['accessLevelId'];
+    $isLoggedIn= $_SESSION['isLoggedIn'];
+    
+    if(isset($_SESSION['govAgencyId']))
+    {
+        //
+        $govtAgencyId= $_SESSION['govAgencyId'];
+    }
+    
+    if(isset($_SESSION['regionId']))
+    {
+        //
+        $regionId= $_SESSION['regionId'];
+    }
+    
+    if(isset($_SESSION['provinceId']))
+    {
+        //
+        $provinceId= $_SESSION['provinceId'];
+    }
+    
+    if(isset($_SESSION['distdivId']))
+    {
+        //
+        $distdivId= $_SESSION['distdivId'];
+    }
+    
+    if(isset($_SESSION['cityMunicipalityId']))
+    {
+        //
+        $cityMunicipalityId= $_SESSION['cityMunicipalityId'];
+    }
+
+    if(isset($_SESSION['barangayId']))
+    {
+        //
+        $barangayId= $_SESSION['barangayId'];
+    }
+
+}
+else
+{
+    session_unset();  
+    session_destroy();
+
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    header("Location:http://$host$uri/");
+}
