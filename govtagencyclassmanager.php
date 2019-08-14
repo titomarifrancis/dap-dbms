649fa72f1e82abb3ff92dbb476ef8207a320e9f0
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-08-15 04:41:48 +0800

    Further refined listing of government agency category

---
+++ govtagencyclassmanager.php
@@ -0,0 +1,43 @@
+<?php
+include 'templates/headerin.php';
+include 'dbconn.php';
+
+//this is to manage the content of Govt Agency Class table
+?>
+<form enctype="multipart/form-data">
+  <div class="row">
+    <div class="large-12 columns">
+		<label>Government Agency Category
+        <table class="scroll hover" style="table-layout:fixed">
+            <thead>
+                <tr>
+                    <th style="width: 100%">Agency Category</th>
+                </tr>
+            </thead>
+            <tbody>
+<?php
+$getGovtAgencyClass=  'select id, agencyclassdesc from govtagencyclass order by agencyclassdesc';
+$govtAgencyClassStmt= $dbh->query($getGovtAgencyClass);
+
+foreach($govtAgencyClassStmt as $govtAgencyClassRow)
+{
+?>
+                <tr> 
+                    <td><a href="<?php echo rtrim($govtAgencyClassRow['id']);?>"><?php echo rtrim($govtAgencyClassRow['agencyclassdesc']);?></a></td>
+                </tr>
+<?php
+}
+?>            
+            </tbody>
+        </table>
+		</label>
+	</div>
+    <div class="large-12 columns">
+        <input type="text" name="govtagencydesc" placeholder="Government Agency Name">
+    </div>
+    <div class="large-12 columns">
+        <input type="submit" class="button expand" value="Save to Add"/>
+    </div>          
+</form>            
+<?
+include 'templates/footer.php';
