21923dbeac4b07d2af04af697345b34720e2c1ad
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-08-16 07:45:50 +0800

    Added PAB Accredited certifying body

---
+++ db/pab-accredited-certifying-body.sql
@@ -0,0 +1,8 @@
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'Brixton Assessment Services', 1, NOW());
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'Bureau Veritas Certification Philippines', 1, NOW());
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'Certification International Philippines, Inc (CIPI)', 1, NOW());
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'SGS Philippines, Inc - CBE', 1, NOW());
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'SOCOTEC Certification Philippines Inc. (formerly AJA Registrars Inc)', 1, NOW());
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'TUV NORD Philippines, Inc.', 1, NOW());
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'TUV Rheinland Philippines, Inc.', 1, NOW());
+insert into certifyingbody(ispabaccredited,	providerorg, creationdate) values(1, 'TUV SUD PSB Philippines Inc.', 1, NOW());
