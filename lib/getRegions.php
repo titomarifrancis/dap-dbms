1730ae5f3e3a45618888d83493fdc129ba809a4c
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-08-26 10:06:29 +0800

    Added utility files to dynamically generate region- and province-related data

---
+++ lib/getRegions.php
@@ -0,0 +1,7 @@
+<?php
+include '../dbconn.php';
+
+$getRegionQuery = 'select id, regionname from regions order by regionname asc';
+$getRegionStmt= $dbh->query($getRegionQuery);
+$regions= $getRegionStmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($regions);
