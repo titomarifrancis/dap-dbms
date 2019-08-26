fa55ab5f5208c6115688a0799d33ad7bd0e16d03
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-08-26 11:56:25 +0800

    Added utility file to list barangay data

--- lib/getBarangay.php
+++ lib/getBarangay.php
@@ -1,26 +1,15 @@
 <?php
-//Utility library file to get the list of 
-//provinces per region and return as list of options in dropdown
 include '../dbconn.php';
 
 if(!isset($_REQUEST['citymunid']))
 {
-    //
     echo 'Usage: getBarangay.php?citymunid=integer_number';
 }
 else
 {
-    //
     $id = $_REQUEST['citymunid'];
     $getBarangayQuery = 'select id, barangayname from barangays where citymunicipalityid='.$id.' order by barangayname';
     $getBarangayQueryStmt= $dbh->query($getBarangayQuery);
     $barangays= $getBarangayQueryStmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($barangays);    
-}
-
-
-/*
-foreach($agencyclassStmt as $agencyclassRow)
-{
-    echo "<option value=".rtrim($agencyclassRow['id']).">".rtrim($agencyclassRow['agencyclassdesc'])."</option>";
 }
