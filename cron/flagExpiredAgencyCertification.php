5ccec2d43720895365224c32fe376e031e8b7001
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-03 14:27:54 +0800

    Updated with shell script shebang and note for non-OpenBSD usage

--- cron/flagExpiredAgencyCertification.php
+++ cron/flagExpiredAgencyCertification.php
@@ -1,9 +1,12 @@
+#! /usr/local/bin/php
 <?php
 include '../dbconn.php';
+
+//the shell script header is OpenBSD-specific, may have to be tweaked for other platforms and environments
 
 $sqlQuery = "update agencycertifications set isexpired=true where certvalidenddate < NOW()";
 $flagAgencyCertExpiredStmt = $dbh->query($sqlQuery);
 $numrecords = $dbh->query($sqlQuery)->rowCount();
 
 //to be implemented:
-//notify thru log or messaging like chat or email of flagged expired agency certification
+//notify thru log or messaging like chat or email of flagged expired agency certification#! 
