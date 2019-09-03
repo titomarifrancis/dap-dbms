1f38fd1ce7372f54dbcd2589c292f11f2921095f
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-03 14:09:29 +0800

    Updated the agency certification history to display from the most recent back to the oldest

--- agencycert_detail.php
+++ agencycert_detail.php
@@ -169,6 +169,47 @@ $agencyCertApprovalArray = $agencyCertStmt->fetchAll();
         </div>
 </form>
 </div>
+<br/>
+<br/>
+<div>
+<h3>Agency Certification History</h3>
+<?php
+//echo $agencyCertApprovalArray[0]['id'];
+//die();
+$agencyId = $agencyCertApprovalArray[0]['id'];
+$getAgenciesQuery = 'select agencycertifications.id as agencycertificationid, agencycertifications.isexpired, govtagency.agencyname as agencyname, certifyingbody.providerorg as certifyingbody, certifications.certificationstandard as certificationdesc, agencycertifications.certvalidstartdate as certstartdate, agencycertifications.certvalidenddate as certenddate, agencycertifications.scope_ispartial as ispartial from govtagencyclass, govtagency, certifyingbody, certifications, agencycertifications where agencycertifications.isapproved=true and agencycertifications.govtagencyid=govtagency.id and agencycertifications.certifyingbodyid=certifyingbody.id and agencycertifications.certificationid=certifications.id and govtagency.govtagencyclassid=govtagencyclass.id and govtagency.id='.$agencyId.' order by agencycertifications.certvalidenddate desc';
+//echo $getAgenciesQuery;
+//die();
+$numrecords = $dbh->query($getAgenciesQuery)->rowCount();
+if($numrecords > 0)
+{
+    //
+    include 'templates/tableheader.php';
+
+    $agencyStmt= $dbh->query($getAgenciesQuery);
+    foreach($agencyStmt as $row)
+    {
+        $isPartial="No";
+        if($row['ispartial'] == 1)
+        {
+            $isPartial="Yes";
+        }
+?>
+                        <tr> 
+                        <td><a href="agencycert_detailsec.php?id=<?php echo $row['agencycertificationid'];?>"><?php echo $row['agencyname'];?></a></td>
+                        <td><?php echo $row['certifyingbody'];?></td>
+                        <td><?php echo $row['certificationdesc'];?></td>
+                        <td><?php echo $row['certstartdate'];?></td>
+                        <td><?php echo $row['certenddate'];?></td>
+                        <td><?php echo $isPartial;?></td>
+                        </tr>          
+<?php
+    }
+    include 'templates/tablefooter.php';    
+}
+?>
+</div>
+
 <?php
 
 include 'templates/footer.php';
