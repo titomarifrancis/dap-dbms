5a3b0d51be11c5456f8d3e070e9bea0c50e200e6
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-04 09:15:12 +0800

    Completed and updated the summary report

--- agencycert_summary.php
+++ agencycert_summary.php
@@ -3,3 +3,92 @@ include 'templates/headerin.php';
 include 'dbconn.php';
 ?>
 <h3>Agency Certification Summary</h3>
+<?php
+include 'templates/summarytableheader.php';
+
+//get agency category
+$getAgencyCategory = "select id, agencyclassdesc from govtagencyclass";
+$getAgencyCategoryStmt = $dbh->query($getAgencyCategory);
+$agencyCategoryArray = $getAgencyCategoryStmt->fetchAll();
+
+foreach($agencyCategoryArray as $categoryRow)
+{
+    $categoryId = $categoryRow['id'];
+    $agencycategoryName = $categoryRow['agencyclassdesc'];
+
+    $getTotalAgencyCount = "select agencyname as totalAgencyCount from govtagency, govtagencyclass where govtagency.govtagencyclassid=govtagencyclass.id and govtagency.govtagencyclassid=$categoryId";
+    $numberTotalAgencyCount = $dbh->query($getTotalAgencyCount)->rowCount();
+    
+    $getActiveCertified = "select agencyname as numActiveCertified from govtagency, agencycertifications where agencycertifications.govtagencyid=govtagency.id and govtagency.govtagencyclassid=$categoryId and agencycertifications.isexpired=false and agencycertifications.isapproved=true";
+    $numberActiveCertified = $dbh->query($getActiveCertified)->rowCount();
+    if($numberTotalAgencyCount == 0)
+    {
+        $percentageActivecertified = "N/A";
+    }
+    else
+    {
+        $percentActivecertified = ($numberActiveCertified/$numberTotalAgencyCount) * 100;
+        $percentageActivecertified = number_format($percentActivecertified, 2);
+
+    }
+
+    $getUncertified = "select distinct govtagency.id from govtagency, govtagencyclass, agencycertifications where govtagencyclass.id=govtagency.govtagencyclassid and agencycertifications.govtagencyid<>govtagency.id and govtagencyclass.id=$categoryId";
+    $numberUncertifiedAgency = $dbh->query($getUncertified)->rowCount();
+    if($numberTotalAgencyCount == 0)
+    {
+        $percentageUncertified = "N/A";
+    }
+    else
+    {
+        $percentUncertified = ($numberUncertifiedAgency/$numberTotalAgencyCount) * 100;
+        $percentageUncertified = number_format($percentUncertified, 2);
+
+    }
+    
+    $numExpired = 0;
+    $getAgencyPerCategory = "select id from govtagency where govtagencyclassid=$categoryId";
+    $getAgencyPerCategoryStmt = $dbh->query($getAgencyPerCategory);
+    $agencyPerCategoryArray = $getAgencyPerCategoryStmt->fetchAll();
+
+    foreach($agencyPerCategoryArray as $agencyPerCategoryArray)
+    {
+        //check if it has expired certification as latest certification
+        $agencyId = $agencyPerCategoryArray['id'];
+        $getLastCertificationStatus = "select agencycertifications.isexpired from govtagencyclass, govtagency, certifyingbody, certifications, agencycertifications where agencycertifications.isapproved=true and agencycertifications.govtagencyid=govtagency.id and agencycertifications.certifyingbodyid=certifyingbody.id and agencycertifications.certificationid=certifications.id and govtagency.govtagencyclassid=govtagencyclass.id and govtagency.id=$agencyId order by agencycertifications.certvalidenddate desc limit 1";
+        $getLastCertificationStatusStmt = $dbh->query($getLastCertificationStatus);
+        $getLastCertificationStatusArray = $getLastCertificationStatusStmt->fetchAll();
+        $lastCertificationStatus = $getLastCertificationStatusArray[0]['isexpired'];
+        if($lastCertificationStatus == 't')
+        {
+            $numExpired++;
+        }
+    }
+    $totalNumberExpiredCertification = $numExpired;
+    if($numberTotalAgencyCount == 0)
+    {
+        $percentageExpired = "N/A";
+    }
+    else
+    {
+        $percentExpired = ($totalNumberExpiredCertification/$numberTotalAgencyCount) * 100;
+        $percentageExpired = number_format($percentExpired, 2);
+
+    }
+
+
+?>
+                            <tr>
+                                <td><?php echo $agencycategoryName;?></td>
+                                <td><?php echo $numberTotalAgencyCount;?></td>
+                                <td><?php echo $numberActiveCertified;?></td>
+                                <td><?php echo $percentageActivecertified;?></td>
+                                <td><?php echo $totalNumberExpiredCertification;?></td>
+                                <td><?php echo $percentageExpired;?></td>
+                                <td><?php echo $numberUncertifiedAgency;?></td>
+                                <td><?php echo $percentageUncertified;?></td>
+                            </tr>
+<?php
+}
+
+include 'templates/tablefooter.php';
+include 'templates/footer.php';
---
+++ templates/summarytableheader.php
@@ -0,0 +1,14 @@
+                    <table class="scroll hover" style="table-layout:fixed">
+                        <thead>
+                            <tr>
+                                <th style="width: 20%">Agency Category</th>
+                                <th style="width: 10%">Total Number of Agencies</th>
+                                <th style="width: 10%">Active Certifications</th>
+                                <th style="width: 10%">% of Active Certifications</th>
+                                <th style="width: 10%">Expired Certifications</th>
+                                <th style="width: 10%">% of Expired Certifications</th>
+                                <th style="width: 10%">Uncertified Agencies</th>
+                                <th style="width: 10%">% of Uncertified Agencies</th>
+                            </tr>
+                        </thead>
+                        <tbody>
