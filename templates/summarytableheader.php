c4bc310ef870bea8e707da3f8a064faad7ba0a3e
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-04 09:24:47 +0800

    Updated some table headers

--- templates/summarytableheader.php
+++ templates/summarytableheader.php
@@ -1,14 +1,14 @@
                     <table class="scroll hover" style="table-layout:fixed">
                         <thead>
                             <tr>
-                                <th style="width: 20%">Agency Category</th>
-                                <th style="width: 10%">Total Number of Agencies</th>
-                                <th style="width: 10%">Active Certifications</th>
-                                <th style="width: 10%">% of Active Certifications</th>
-                                <th style="width: 10%">Expired Certifications</th>
-                                <th style="width: 10%">% of Expired Certifications</th>
-                                <th style="width: 10%">Uncertified Agencies</th>
-                                <th style="width: 10%">% of Uncertified Agencies</th>
+                                <th style="width: 23%">Agency Category</th>
+                                <th style="width: 11%">Total Number of Agencies</th>
+                                <th style="width: 11%">Active Certifications</th>
+                                <th style="width: 11%">Active Certifications (%)</th>
+                                <th style="width: 11%">Expired Certifications</th>
+                                <th style="width: 11%">Expired Certifications (%)</th>
+                                <th style="width: 11%">Uncertified Agencies</th>
+                                <th style="width: 11%">Uncertified Agencies (%)</th>
                             </tr>
                         </thead>
                         <tbody>
