971a86fa2ccc2b8f4fca6db6fe3882eefe14f427
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-10 18:07:06 +0800

    Updated query to display the expected order of regions

--- lib/getRegions.php
+++ lib/getRegions.php
@@ -1,7 +1,7 @@
 <?php
 include '../dbconn.php';
 
-$getRegionQuery = 'select id, regionname from regions order by regionname asc';
+$getRegionQuery = 'select id, regionname from regions order by id asc';
 $getRegionStmt= $dbh->query($getRegionQuery);
 $regions= $getRegionStmt->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($regions);
