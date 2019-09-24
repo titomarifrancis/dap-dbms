65329fb592888ee688cafb72d4a20446f6b0e46c
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-24 14:27:13 +0800

    Updated to satisfy reqt for Logout be located near bottom of the menu

--- templates/sidebarin.php
+++ templates/sidebarin.php
@@ -1,18 +1,6 @@
 <?php
 ?>
-                <!-- Dynamic auth sidebar start -->
-                <aside id="custom_html-3" class="widget_text widget callout secondary widget_custom_html">
-                    <div class="entry-content">
-                        <style>
-                            ul.sidebar-sl li { margin-bottom: 0.5rem }
-                            a.sidebar-slink { font-size: 1.05rem; padding: 0.5rem 1rem; color: #fff; background: #2e3192; display: block; border-radius: 3px }
-                            a.sidebar-slink:hover { background: #4145c7; color: #fff !important }
-                        </style>
-                        <ul style="list-style: none; margin: 0" class="sidebar-sl">
-                            <li><a href="logout.php" class="sidebar-slink">Logout</a></li>
-                        </ul>
-                    </div>
-                </aside>
+
                 <!-- Dynamic auth sidebar end -->
                 <aside id="custom_html-3" class="widget_text widget callout secondary widget_custom_html">
                     <div class="entry-content">
@@ -23,11 +11,12 @@
                             </style>
                             <ul style="list-style: none; margin: 0" class="sidebar-sl">
                                 <li class="sidebar-sl"><a href="agencycertification.php" class="sidebar-slink">Agency Certification Manager</a></li>
-                                <li class="sidebar-sl"><a href="certifyingbodymanager.php" class="sidebar-slink">Certifying Body Manager</a></li>
+                                
 <?php
 if(isset($loggedInAccessLevel) && ($loggedInAccessLevel >= 2))
 {
-?>                                
+?>                              
+                                <li class="sidebar-sl"><a href="certifyingbodymanager.php" class="sidebar-slink">Certifying Body Manager</a></li>
                                 <li class="sidebar-sl"><a href="govtagencyclassmanager.php" class="sidebar-slink">Government Agency Category Manager</a></li>
                                 <li class="sidebar-sl"><a href="govtagencymanager.php" class="sidebar-slink">Government Agency Manager</a></li>
                                 
@@ -52,3 +41,16 @@ if(isset($loggedInAccessLevel) && ($loggedInAccessLevel >= 2))
                             </ul>
                     </div>
                 </aside>
+                <!-- Dynamic auth sidebar start -->
+                <aside id="custom_html-3" class="widget_text widget callout secondary widget_custom_html">
+                    <div class="entry-content">
+                        <style>
+                            ul.sidebar-sl li { margin-bottom: 0.5rem }
+                            a.sidebar-slink { font-size: 1.05rem; padding: 0.5rem 1rem; color: #fff; background: #2e3192; display: block; border-radius: 3px }
+                            a.sidebar-slink:hover { background: #4145c7; color: #fff !important }
+                        </style>
+                        <ul style="list-style: none; margin: 0" class="sidebar-sl">
+                            <li><a href="logout.php" class="sidebar-slink">Logout</a></li>
+                        </ul>
+                    </div>
+                </aside>                
