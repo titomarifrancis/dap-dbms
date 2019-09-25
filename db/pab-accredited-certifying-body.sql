8c3a571cf429745136867e7faaacfa11e7227f5c
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-25 14:05:49 +0800

    Updated with removal of text as per request from project manager

--- db/pab-accredited-certifying-body.sql
+++ db/pab-accredited-certifying-body.sql
@@ -2,7 +2,7 @@ insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdat
 insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'Bureau Veritas Certification Philippines', 'TRUE', 'NOW()');
 insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'Certification International Philippines, Inc (CIPI)', 'TRUE', 'NOW()');
 insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'SGS Philippines, Inc - CBE', 'TRUE', 'NOW()');
-insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'SOCOTEC Certification Philippines Inc. (formerly AJA Registrars Inc)', 'TRUE', 'NOW()');
+insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'SOCOTEC Certification Philippines Inc.', 'TRUE', 'NOW()');
 insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'TUV NORD Philippines, Inc.', 'TRUE', 'NOW()');
 insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'TUV Rheinland Philippines, Inc.', 'TRUE', 'NOW()');
 insert into certifyingbody(ispabaccredited,	providerorg, isapproved, creationdate) values('TRUE', 'TUV SUD PSB Philippines Inc.', 'TRUE', 'NOW()');
