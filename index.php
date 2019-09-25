f6052805537fa95bd34daf56b3b63f8155577d88
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-09-25 11:45:47 +0800

    Updated the home page as per content provided by project manager

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
 include 'templates/header.php';
 ?>
-Home page under construction
+<h3>GOVERNMENT QUALITY MANAGEMENT PROGRAM</h3>
+<p>
+The Government Quality Management Program, created through the Executive Order No. 605, Institutionalizing the Structure, Mechanisms, and Standards to Implement the Government Quality Management Program (GQMP), aims to enhance citizen satisfaction and public sector productivity.  
+</p>
+<p>
+Through government-wide effort to improve its delivery of services to the Filipino people, government agencies are directed to streamline their processes and rationalize documentary requirements for the ease of transaction of the citizens. The Philippine Development Plan 2017-2022, specifically in Chapter 5, emphasizes the need to build a responsive and transparent public sector through citizen-centered, clean and efficient delivery of public goods and services. 
+</p>
+<p>
+The Government Quality Management Program (GQMP) aims to:<br/>
+
+    1. Increase the number of agencies with Quality Management System certified to ISO 9001:2015;<br/>
+    2. Streamline processes of frontline services;<br/>
+    3. Establish and promote implementation of service quality standards;<br/>
+    4. Improve the workplace quality and organization of the national government agencies;<br/>
+    5. Develop and promote innovations in public service delivery; and,<br/>
+    6. Strengthen capacities of government agencies in adopting quality improvement approaches.<br/>
+</p>
+<p>
+The Government Quality Management Program (GQMP), in its continuous effort to concentrate on improving the frontline services of the government, has five main components:<br/>
+
+        1. Strengthening and expanding the government quality management system<br/>
+        2. Institutionalizing the 5S good housekeeping program<br/>
+        3. Institutionalizing the Service Quality Standards<br/>
+        4. Public Sector Innovation Laboratory<br/>
+        5. Promoting public sector productivity and quality improvement<br/>
+</p>
 <?php
 include 'templates/footer.php';
