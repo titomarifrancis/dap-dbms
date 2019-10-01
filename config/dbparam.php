dbf2521102c436f6f25f2d94d95113765b45254e
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-10-01 15:45:14 +0800

    Removed unneeded files replaced by listcertification

--- config/dbparam.php
+++ config/dbparam.php
@@ -1,8 +1,8 @@
 <?php
 //dev server
 $deployType="Dev";
-$hName = "192.168.227.130";
+$hName = "192.168.117.133";
 $dbPort = "5432";
-$dbName = "edgekit_dap";
+$dbName = "dap_edgekit";
 $userName = "dap_edgekit";
 $passWord = "password";
