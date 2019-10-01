5e4a6b5c287b9c00c6aa94dbd083db40bca13c0d
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-10-01 21:24:36 +0800

    New table listing view templates

---
+++ templates/agencylistheader.php
@@ -0,0 +1,7 @@
+                    <table class="scroll hover" style="table-layout:fixed">
+                        <thead>
+                            <tr>
+                                <th style="width: 100%">Agency Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
