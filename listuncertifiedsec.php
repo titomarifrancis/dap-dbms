62a0a74a8cb535786f89f51edea107fc01251059
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-10-01 23:32:45 +0800

    Code cleanup for better readability

--- listuncertifiedsec.php
+++ listuncertifiedsec.php
@@ -10,8 +10,6 @@ $agencyCategoryLabel = $getCategoryStmt[0]['agencyclassdesc'];
 <h3>Uncertified <?php echo $agencyCategoryLabel;?></h3>
 <br/>
 <?php
-//$getAgenciesQuery = "select agencycertifications.id as agencycertificationid, govtagency.agencyname as agencyname, certifyingbody.providerorg as certifyingbody, certifications.certificationstandard as certificationdesc, agencycertifications.certvalidstartdate as certstartdate, agencycertifications.certvalidenddate as certenddate, agencycertifications.scope_ispartial as ispartial from govtagencyclass, govtagency, certifyingbody, certifications, agencycertifications where agencycertifications.isapproved=true and agencycertifications.govtagencyid=govtagency.id and agencycertifications.certifyingbodyid=certifyingbody.id and agencycertifications.certificationid=certifications.id and govtagency.govtagencyclassid=govtagencyclass.id and agencycertifications.isexpired=false and govtagencyclass.id=$agencycategoryId order by agencyname";
-//$getAgenciesQuery = "select govtagency.agencyname as agencyname from govtagencyclass, govtagency, certifyingbody, certifications, agencycertifications where agencycertifications.isapproved=true and agencycertifications.govtagencyid<>govtagency.id and agencycertifications.certifyingbodyid=certifyingbody.id and agencycertifications.certificationid=certifications.id and govtagency.govtagencyclassid=govtagencyclass.id and agencycertifications.isexpired=false and govtagencyclass.id=$agencycategoryId order by agencyname";
 $getAgenciesQuery = "select distinct govtagency.id as agencyid, govtagency.agencyname from govtagencyclass, govtagency, agencycertifications where agencycertifications.isapproved=true and agencycertifications.govtagencyid!=govtagency.id and govtagency.govtagencyclassid=govtagencyclass.id and agencycertifications.isexpired=true and govtagencyclass.id=$agencycategoryId order by govtagency.agencyname";
 $numrecords = $dbh->query($getAgenciesQuery)->rowCount();
 if($numrecords > 0)
@@ -32,9 +30,9 @@ if($numrecords > 0)
 
         //check that this agency has no certification record
         $checkIfUncertified = "select govtagency.id, govtagency.agencyname as numActiveCertified from govtagency, agencycertifications where agencycertifications.govtagencyid=govtagency.id  and agencycertifications.isexpired=false and agencycertifications.isapproved=true and govtagency.id=$govtAgencyId";
-        //echo "$checkIfUncertified<br/>";
         $checkUncertifiedCount = $dbh->query($checkIfUncertified)->rowCount();
 
+        //show only those not certified
         if($checkUncertifiedCount <= 0)
         {
             //
@@ -44,10 +42,12 @@ if($numrecords > 0)
             </tr>          
         <?php            
         }
-
-
     }
-    include 'templates/tablefooter.php';    
+    include 'templates/tablefooter.php';
+?>
+    <br/>
+    <a href="reports/agencycert_summary_excel.php" target="_tab"><input type="button" class="button expand" id="okButton" name="uploadBtn" value="Export as Excel"></a>&nbsp;&nbsp;<a href="reports/agencycert_summary_pdf.php" target="_tab"><input type="button" class="button expand" id="okButton" name="uploadBtn" value="Export as PDF"></a>
+<?php    
 }
 else
 {
