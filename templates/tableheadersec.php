0e8d617a6f17dbeb1a10595f78dbf5eef16a0680
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-10-08 23:17:29 +0800

    Separated table header for logged in user, for more information and reports

---
+++ templates/tableheadersec.php
@@ -0,0 +1,16 @@
+                    <table class="scroll hover" style="table-layout:fixed">
+                        <thead>
+                            <tr>
+                                <th style="width: 10%">Name of Agency</th>
+                                <th style="width: 10%">Certifying Body</th>
+                                <th style="width: 10%">Certification</th>
+                                <th style="width: 10%">Valid From</th>
+                                <th style="width: 10%">Valid Until</th>
+                                <th style="width: 10%">Original Certification Date</th>
+                                <th style="width: 10%">Scope of Certification</th>
+                                <th style="width: 10%">Region</th>
+                                <th style="width: 10%">Province</th>
+                                <th style="width: 10%">City/Municipality</th>
+                            </tr>
+                        </thead>
+                        <tbody>
