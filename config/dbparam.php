613c3d01db8428cb5b150dbb93d988f81b9bf7dc
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2019-10-10 11:41:13 +0800

    Update dbparam.php
    To enable VM setup in DAP

--- config/dbparam.php
+++ config/dbparam.php
@@ -1,7 +1,8 @@
 <?php
 //dev server
 $deployType="Dev";
-$hName = "192.168.117.133";
+#DAP VM IP: 192.168.227.130
+$hName = "192.168.227.130";
 $dbPort = "5432";
 $dbName = "dap_edgekit";
 $userName = "dap_edgekit";
