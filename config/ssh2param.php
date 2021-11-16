53a30bb1cae46a02f59eab5b7ed2ad21b0bbf1a4
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2021-11-17 07:55:21 +0800

    Updated with subdomain for cert fileserver URL

--- config/ssh2param.php
+++ config/ssh2param.php
@@ -1,9 +1,11 @@
 <?php
 $upload_path = "stash/";
 //$upload_path = "/var/www/htdocs/dap-dbms/stash/";
-$ssh2host = "192.168.253.160";;
+$ssh2host = "119.92.118.27";
 $ssh2port = "22";
-$ssh2username = "root";
-$ssh2password = "password";
-$ssh2destpath = "/var/www/htdocs/stash/";
+$ssh2username = "titomarifrancis";
+$ssh2password = "431211m@s@y@";
+$ssh2destpath = "/var/www/html/stash/";
+$ssh2hostalias = "";
+//$ssh2hostalias = "agencycertviewer.dap.edu.ph";
 //$ssh2destpath = "/home/titomarifrancis/stash/";
