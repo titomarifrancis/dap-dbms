aa0ba3f2988ca5ba6081f1fc13913361b6f00b8c
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2021-12-17 22:51:59 +0800

    Changed 'Uncertified Agencies' to 'Agencies Without ISO Certification'

--- templates/summarytableheader.php
+++ templates/summarytableheader.php
@@ -1,12 +1,12 @@
-                    <table class="scroll hover" style="table-layout:fixed">
+<table class="scroll hover" style="table-layout:fixed">
                         <thead>
                             <tr>
                                 <th style="width: 34%">Type of Government Agency</th>
                                 <th style="width: 11%">No. of Agencies Certified</th>
                                 <th style="width: 11%">Active Certifications</th>
                                 <th style="width: 11%">Active Certifications (%)</th>
-                                <th style="width: 11%">Uncertified Agencies</th>
-                                <th style="width: 11%">Uncertified Agencies (%)</th>
+                                <th style="width: 11%">Agencies Without ISO Certification</th>
+                                <th style="width: 11%">Agencies Without ISO Certification (%)</th>
                                 <th style="width: 11%">Expired Certifications</th>                                
                             </tr>
                         </thead>
