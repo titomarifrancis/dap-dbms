dfe2a072107014c7d8670ffcf5b470fbc82336a7
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-01-12 23:22:59 +0800

    Code cleanup

--- agencycertification.php
+++ agencycertification.php
@@ -10,7 +10,6 @@ if(isset($_REQUEST['msg']))
 	unset($_REQUEST['msg']);
 	if($msg_data == 1)
 	{
-		//
 		?>
 		<div data-alert class="alert-box" tabindex="0" aria-live="assertive" role="alertdialog">
 		Congratulations for successfully recording the certification of your agency.<br/>
@@ -21,13 +20,11 @@ if(isset($_REQUEST['msg']))
 	}
 	elseif($msg_data == 2)
 	{
-		//
 		?>
 		<div data-alert class="alert-box" tabindex="0" aria-live="assertive" role="alertdialog">
 		Please properly fill-in all the required form fields<br/>
 		</div>
 		<?php
-		//unset($_REQUEST['msg']);	
 	}
 
 }
@@ -182,7 +179,6 @@ if(isset($_REQUEST['msg']))
 
 	<div class="large-12 columns">
 		<label>Scope of Certification  &nbsp;
-			<!--<input type="checkbox" name="scope_ispartial" id="scope_ispartial" placeholder="Certification Scope Is Partial?">-->
 			<input type="radio" name="scope_ispartial" value="true">Partial &nbsp;&nbsp;<input type="radio" name="scope_ispartial" value="false">Full<br>
 		</label>
 	</div>	
@@ -391,37 +387,6 @@ if(isset($loggedInAccessLevel) && ($loggedInAccessLevel > 1))
 		include 'templates/tablefooter.php';
 	}
 	
-	/*
-	$getUnapprovedAgencyCert = "select agencycertifications.id as agencycertid, govtagency.agencyname as agencyname, certifyingbody.providerorg as certifyingbody, certifications.certificationstandard as certificationdesc, agencycertifications.certvalidstartdate as certstartdate, agencycertifications.certvalidenddate as certenddate, agencycertifications.scope_ispartial as ispartial from govtagencyclass, govtagency, certifyingbody, certifications, agencycertifications where agencycertifications.isapproved=false and agencycertifications.govtagencyid=govtagency.id and agencycertifications.certifyingbodyid=certifyingbody.id and agencycertifications.certificationid=certifications.id and govtagency.govtagencyclassid=govtagencyclass.id order by agencyname";
-	//echo $getUnapprovedAgencyCert;
-	//die();
-	$unapprovedAgencyStmt = $dbh->query($getUnapprovedAgencyCert);
-	$numrecords = $unapprovedAgencyStmt->rowcount();
-	//if($numrecords > 0)
-	//{
-		echo "<h3>Agency Certification Requiring Approval</h3>";
-		include 'templates/tableheader.php';
-		foreach($unapprovedAgencyStmt as $unapprovedAgencyRow)
-		{
-			$isPartial="No";
-			if($unapprovedAgencyRow['ispartial'] == 1)
-			{
-				$isPartial="Yes";
-			}
-		?>
-							<tr> 
-							<td><a href="agencycert_approval.php?id=<?php echo $unapprovedAgencyRow['agencycertid'];?>"><?php echo $unapprovedAgencyRow['agencyname'];?></a></td>
-							<td><?php echo $unapprovedAgencyRow['certifyingbody'];?></td>
-							<td><?php echo $unapprovedAgencyRow['certificationdesc'];?></td>
-							<td><?php echo $unapprovedAgencyRow['certstartdate'];?></td>
-							<td><?php echo $unapprovedAgencyRow['certenddate'];?></td>
-							<td><?php echo $isPartial;?></td>
-							</tr>          
-		<?php
-		}
-		include 'templates/tablefooter.php';
-	//}
-	*/
 }
 ?>
 </div>
@@ -482,30 +447,7 @@ const headofagency = document.getElementById('headofagency');
 const certvalidstartdate = document.getElementById('certvalidstartdate');
 const certvalidenddate = document.getElementById('certvalidenddate');
 const uploadedFile = document.getElementById('uploadedFile');
-/*
-certForm.addEventListener('keyup', function (event)
-{
-    isValidGovtAgency = govtagencyField.checkValidity();
-    isValidCertification = certificationField.checkValidity();
-    isValidCertifyingBody = certifyingbodyField.checkValidity();
-    isValidCertificationRegnumber = certificationregnumber.checkValidity();
-	isvalidCertificationScope = certificationscope.checkValidity();
-	isvalidHeadofAgency = headofagency.checkValidity();
-	isvalidCertStartDate = certvalidstartdate.checkValidity();
-	isValidCertEndDate = certvalidenddate.checkValidity();
-    isValidUploadedFile = uploadedFile.checkValidity();
 
-    
-    if ( isValidGovtAgency && isValidCertification && isValidCertifyingBody && isValidCertificationRegnumber && isvalidCertificationScope && isvalidHeadofAgency && isvalidCertStartDate && isValidCertEndDate && isValidUploadedFile )
-    {
-        okButton.disabled = false;
-    }
-    else
-    {
-        okButton.disabled = true;
-    }
-});
-*/
 okButton.addEventListener('click', function (event)
 {
 	certForm.submit();
