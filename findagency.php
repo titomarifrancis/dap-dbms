54ea73418360933a470b8616a6851f026befce24
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-01-12 23:30:12 +0800

    Updated the input form to find agency

--- findagency.php
+++ findagency.php
@@ -4,6 +4,15 @@ include 'templates/header.php';
 include 'dbconn.php';
 ?>
 <h3>Find Agency</h3>
+<form id="certForm" method="post" action="agencycert_processor.php" enctype="multipart/form-data">
+  <div class="row">
+    <div class="large-12 columns">
+      <label>Full or Partial Name of the Agency
+        <input type="text" name="certificationsite" id="certificationsite" placeholder="Type the Site of Certification Here">
+      </label>
+    </div>
+	</div>
+</form>
 
 <?php
 include 'templates/footer.php';
