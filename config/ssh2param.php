65d699316030cda79bda16f84a75a95a8a3feeb8
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-03 16:14:43 +0800

    Updated params for refinement

--- config/ssh2param.php
+++ config/ssh2param.php
@@ -1,10 +1,10 @@
 <?php
 $upload_path = "stash/";
 //$upload_path = "/var/www/htdocs/dap-dbms/stash/";
-$ssh2host = "119.92.118.27";
+$ssh2host = "192.168.253.144";
 $ssh2port = "22";
 $ssh2username = "titomarifrancis";
-$ssh2password = "431211m@s@y@";
+$ssh2password = "password";
 $ssh2destpath = "/var/www/html/stash/";
 $ssh2hostalias = "";
 //$ssh2hostalias = "agencycertviewer.dap.edu.ph";
