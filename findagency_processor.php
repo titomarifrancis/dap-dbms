fe9319d416df2a746c2c957a36e465fb10004721
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-05 10:09:29 +0800

    Logic loop test

--- findagency_processor.php
+++ findagency_processor.php
@@ -6,12 +6,10 @@ include 'dbconn.php';
 partfullagencyname
 if((isset($_REQUEST['partfullagencyname']))
 {
-  echo "This is a test";
-}
 ?>
-<!--
 <h3>Agency Search Result</h3>
 This is a test
 
 <?php
+}
 include 'templates/footer.php';
