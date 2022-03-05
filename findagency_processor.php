b99f5fbb80b90e322a577a5cfbcc8a8143000813
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-05 10:39:48 +0800

    Echo input string test

--- findagency_processor.php
+++ findagency_processor.php
@@ -2,12 +2,11 @@
 //find agency processor
 include 'templates/header.php';
 include 'dbconn.php';
-
-if(isset($_REQUEST['partfullagencyname']))
-{
 ?>
 <h3>Agency Search Result</h3>
 <?php
- echo "Input string is $_REQUEST['partfullagencyname']";
+if(isset($_REQUEST['partfullagencyname']))
+{
+  echo "Input string is $_REQUEST['partfullagencyname']";
 }
 include 'templates/footer.php';
