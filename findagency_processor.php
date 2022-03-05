d83308a9381278bb90bf74df3b547059dafab7b2
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-05 10:40:59 +0800

    Minor change to verify input string is passed

--- findagency_processor.php
+++ findagency_processor.php
@@ -2,9 +2,7 @@
 //find agency processor
 include 'templates/header.php';
 include 'dbconn.php';
-?>
-<h3>Agency Search Result</h3>
-<?php
+
 if(isset($_REQUEST['partfullagencyname']))
 {
   echo "Input string is $_REQUEST['partfullagencyname']";
