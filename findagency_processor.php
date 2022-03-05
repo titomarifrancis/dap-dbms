cf01306c48ec0974bb7ee55964ba996216b13bc2
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-05 12:00:16 +0800

    Minor changes in the logic, still working on it

--- findagency_processor.php
+++ findagency_processor.php
@@ -11,7 +11,7 @@ if(isset($_REQUEST['partfullagencyname']))
   //echo "Input string is $inputString<br/>";
 
   $query= "select agencycertifications.id as agencycertificationid, govtagency.agencyname as agencyname, certifyingbody.providerorg as certifyingbody, certifications.certificationstandard as certificationdesc, agencycertifications.certvalidstartdate as certstartdate, agencycertifications.certvalidenddate as certenddate, agencycertifications.scope_ispartial as ispartial, regions.regionname, provinces.provincename, citymunicipality.towncitymunicipalityname from govtagencyclass, govtagency, certifyingbody, certifications, agencycertifications, regions, provinces, citymunicipality where agencycertifications.isapproved=true and agencycertifications.govtagencyid=govtagency.id and agencycertifications.certifyingbodyid=certifyingbody.id and agencycertifications.certificationid=certifications.id and govtagency.govtagencyclassid=govtagencyclass.id and agencycertifications.isexpired=false and agencycertifications.regionid=regions.id and agencycertifications.provinceid=provinces.id and agencycertifications.citymunicipalityid=citymunicipality.id and govtagency.agencyname like '%$inputString%' order by agencyname";
-  //echo $query;
+  echo "$query<br/>";
   $queryCount= $dbh->query($getAgenciesQueryNational)->rowCount();
   if($queryCount > 0)
   {
