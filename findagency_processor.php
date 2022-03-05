7d69071721a803594b5e2e0b6655705e113a760b
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-05 10:38:29 +0800

    Echo input string

--- findagency_processor.php
+++ findagency_processor.php
@@ -7,8 +7,7 @@ if(isset($_REQUEST['partfullagencyname']))
 {
 ?>
 <h3>Agency Search Result</h3>
-This is a test
-
 <?php
+ echo "Input string is $_REQUEST['partfullagencyname']";
 }
 include 'templates/footer.php';
