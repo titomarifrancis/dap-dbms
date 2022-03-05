134423d23fcb419f03cad4cbcc4f96cc8ce16a07
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-05 11:47:51 +0800

    Developing the query

--- findagency_processor.php
+++ findagency_processor.php
@@ -8,6 +8,9 @@ include 'dbconn.php';
 if(isset($_REQUEST['partfullagencyname']))
 {
   $inputString= ucwords($_REQUEST['partfullagencyname']);
-  echo "Input string is $inputString";
+  echo "Input string is $inputString<br/>";
+
+  $query= select agencycertifications.id as agencycertificationid, govtagency.agencyname as agencyname, certifyingbody.providerorg as certifyingbody, certifications.certificationstandard as certificationdesc, agencycertifications.certvalidstartdate as certstartdate, agencycertifications.certvalidenddate as certenddate, agencycertifications.scope_ispartial as ispartial, regions.regionname, provinces.provincename, citymunicipality.towncitymunicipalityname from govtagencyclass, govtagency, certifyingbody, certifications, agencycertifications, regions, provinces, citymunicipality where agencycertifications.isapproved=true and agencycertifications.govtagencyid=govtagency.id and agencycertifications.certifyingbodyid=certifyingbody.id and agencycertifications.certificationid=certifications.id and govtagency.govtagencyclassid=govtagencyclass.id and agencycertifications.isexpired=false and agencycertifications.regionid=regions.id and agencycertifications.provinceid=provinces.id and agencycertifications.citymunicipalityid=citymunicipality.id and govtagency.agencyname like '%$inputString%' order by agencyname;
+  echo $query;
 }
 include 'templates/footer.php';
