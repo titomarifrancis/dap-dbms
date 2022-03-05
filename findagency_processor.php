d82b7bbe13dffe3ff530801a8c018d0d159c06f9
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-05 10:13:18 +0800

    Fixed excess parenthesis

--- findagency_processor.php
+++ findagency_processor.php
@@ -3,7 +3,7 @@
 include 'templates/header.php';
 include 'dbconn.php';
 
-if((isset($_REQUEST['partfullagencyname']))
+if(isset($_REQUEST['partfullagencyname']))
 {
 ?>
 <h3>Agency Search Result</h3>
