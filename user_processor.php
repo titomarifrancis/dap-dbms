7cca6126c9b46b3b0bb7d8effcacb7a5fcb4f931
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-03-15 12:21:04 +0800

    Updated SMTP parameters, for testing

--- user_processor.php
+++ user_processor.php
@@ -508,12 +508,13 @@ if($isapproved == 'true')
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPSecure = 'static::ENCRYPTION_STARTTLS';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'titomarifrancis@gmail.com';                     //SMTP username
         $mail->Password   = '431211m@s4yA431211';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
         $mail->setFrom($from, $fromName);
