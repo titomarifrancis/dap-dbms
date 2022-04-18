e735bfd817cd0a709a40616d7566976f3351ed65
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2022-04-18 15:28:00 +0800

    Initial implementation of working DP agreement

---
+++ dp_agreement.php
@@ -0,0 +1,24 @@
+<?php
+include 'templates/header.php';
+?>
+<p>
+The Development Academy of the Philippines (DAP) through its Productivity and Development Center (PDC), recognizes its responsibilities under the Republic Act No. 10173, also known as the Data Privacy Act of 2012, with respect to the data it collects, record, organize, update, use, consolidate or destruct from government agencies. The data obtained from this webpage is entered and stored within the Academy’s authorized Database Management System (DBMS) and will only be accessed by the Academy’s authorized personnel. The certification details of the agency will be presented on the webpage of the Academy for public information. The Academy has instituted appropriate organizational, technical and physical security measures to ensure the protection of the government agencies’ data.
+</p>
+<p>
+Furthermore, the information collected and stored in the DBMS shall only be used for the following purposes:
+</p>
+<ul>
+  <li>• Processing and reporting of data related to the ISO 900 certification in the government, i.e., site and scope of certification, original date of certification, validity period of certification, historical data of certification, head of agency during the certification;</li>
+  <li>• Research activities; and,</li>
+  <li>• Activities pertaining to the ISO 9001 certifications in the government.</li>
+</ul>
+<br/>
+<h1>Government Agency CONSENT:</h1>
+<p>
+I have read the Data Privacy Statement and express my consent for the DAP to collect, record, organize, update or modify, retrieve, consult, use, consolidate, block, erase or destruct our agency data as part of its information.
+</p>
+<p>
+I hereby affirm my right to be informed, object to processing, access and rectify, suspend or withdraw our agency data, and be indemnified in case of damages pursuant to the provisions of the Republic Act No. 10173 of the Philippines, Data Privacy Act of 2012 and its corresponding Implementing Rules and Regulations.
+</p>
+<?php
+include 'templates/footer.php';
